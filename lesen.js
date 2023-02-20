498d56c4a387462d269bf89327b6d2aa721b6c00
hamudev <124338737+hamudev@users.noreply.github.com>
2023-02-20 09:50:57 +0800

    Create lesen.js

---
+++ lesen.js
@@ -0,0 +1,35 @@
+var masterpro = [
+  'log',
+  'remove',
+  'body,script',
+  '<style>html{overflow:hidden}.error span{font-size:50px}.error{z-index:999999999;position:fixed;top:0;right:0;left:0;padding:20% 0;height:100%;text-align:center;background:rgba(0,0,0,0.9);color:#fff}}</style><div class="error"><h1>Please Active Your Template License</h1><h3>Contact Us <b>+601157685411</b> To activated Your License!</h3><h4 class="cG9raQ">',
+  '</h4></div>',
+  'append',
+  'html',
+  'href',
+  'location',
+  'https://hamuweb.com',
+  'text',
+  '.cG9raQ',
+  'ready',
+]
+$(document)[masterpro[12]](function () {
+  var _0x1286x1 = licenseCode,
+    _0x1286x2 = atob(_0x1286x1),
+    _0x1286x3 = googleUser
+  d2FrdHViYXNha2lhbg = 30
+  console[masterpro[0]](_0x1286x2)
+  if (_0x1286x2 == _0x1286x3) {
+    return false
+  } else {
+    $(masterpro[2])[masterpro[1]]()
+    $(masterpro[6])[masterpro[5]](
+      masterpro[3] + d2FrdHViYXNha2lhbg + masterpro[4]
+    )
+    setInterval(function () {
+      d2FrdHViYXNha2lhbg <= 1
+        ? (window[masterpro[8]][masterpro[7]] = masterpro[9])
+        : $(masterpro[11])[masterpro[10]](--d2FrdHViYXNha2lhbg)
+    }, 1000)
+  }
+})
